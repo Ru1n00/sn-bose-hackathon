3fae6b98ca89696046357931c602a1b8bfd209d5
Ru1n00 <ruhul.amin.tnt@gmail.com>
2025-05-21 02:44:28 +0600

    feat: Add content urls to urlpatterns, change admin texts

--- main_app/core/urls.py
+++ main_app/core/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from core.settings import DEBUG, MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('content.urls')),
+] 
+
+if DEBUG:
+    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+# admin settings
+admin.site.site_header = "Edutech"
+admin.site.site_title = "Edutech Admin"
+admin.site.index_title = "Welcome to Edutech Admin"
