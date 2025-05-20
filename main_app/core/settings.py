4b83b4e5ac112d506ae3b05657c0b06f4e1637fc
Ru1n00 <ruhul.amin.tnt@gmail.com>
2025-05-20 20:00:17 +0600

    feat: Config static files and media files directory

--- main_app/core/settings.py
+++ main_app/core/settings.py
@@ -122,8 +122,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
