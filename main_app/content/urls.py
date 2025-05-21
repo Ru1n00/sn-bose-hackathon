9426498e539f9a6234e2934d90b4ec528edf635d
Ru1n00 <ruhul.amin.tnt@gmail.com>
2025-05-21 07:28:34 +0600

    feat: Add create post functionality

--- main_app/content/urls.py
+++ main_app/content/urls.py
@@ -4,6 +4,7 @@ from .views import (
     index,
     PostListView,
     post_detail,
+    post_create,
 )
 
 app_name = 'content'
@@ -11,5 +12,6 @@ app_name = 'content'
 urlpatterns = [
     path('', index, name='index'),
     path('posts/', PostListView.as_view(), name='post_list'),
-    path('post/<str:slug>', post_detail, name='post_detail'),
+    path('post/<str:slug>/', post_detail, name='post_detail'),
+    path('post-create/', post_create, name='post_create'),
 ]
--- main_app/content/views.py
+++ main_app/content/views.py
@@ -7,6 +7,8 @@ from .models import (
     PostFile,
 )
 
+from .forms import PostForm, PostFileFormSet
+
 # Create your views here.
 def index(request):
     return render(request, 'content/index.html')
@@ -41,10 +43,41 @@ def post_detail(request, slug):
         translated_description = translate_text(post.description)
         post.title = translated_title
         post.description = translated_description
-        
+
     context = {
         'post': post,
         'videos': videos,
         'images': images,
     }
     return render(request, 'content/post_detail.html', context)
+
+
+def post_create(request):
+    if request.method == 'POST':
+        # Handle form submission
+        form = PostForm(request.POST)
+
+        # Handle file uploads
+        file_form = PostFileFormSet(request.POST, request.FILES)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.post_user = request.user
+            post.save()
+            # Handle file uploads
+            file_form = PostFileFormSet(request.POST, request.FILES, instance=post)
+
+            for form in file_form:
+                if form.is_valid():
+                    if form.cleaned_data.get('file'):
+                        post_file = form.save(commit=False)
+                        post_file.post = post
+
+                        post_file.save()
+            return redirect('content:post_list')
+    else:
+        form = PostForm()
+        # Handle file uploads
+        file_form = PostFileFormSet()
+
+    return render(request, 'content/post_create.html', {'form': form, 'file_form': file_form})
