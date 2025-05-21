26a8f18de3802de4e6cf6a385279694ffa6baa51
Ru1n00 <ruhul.amin.tnt@gmail.com>
2025-05-21 06:14:53 +0600

    feat: Add post list view

--- main_app/content/urls.py
+++ main_app/content/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     index,
+    PostListView,
 )
 
 app_name = 'content'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('posts/', PostListView.as_view(), name='post_list'),
 ]
--- main_app/content/views.py
+++ main_app/content/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import (
+    Post
+)
 
 # Create your views here.
 def index(request):
     return render(request, 'content/index.html')
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'content/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 10  # Number of posts per page
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
