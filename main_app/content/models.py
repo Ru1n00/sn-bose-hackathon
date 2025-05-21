e0af9e6ca888c99a94a2150345d991a0c687b334
Ru1n00 <ruhul.amin.tnt@gmail.com>
2025-05-21 08:16:48 +0600

    feat: Update Post model, prepare to make inactive if gets reports_count > 10

--- main_app/content/models.py
+++ main_app/content/models.py
@@ -100,7 +100,7 @@ class Post(models.Model):
     report_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         ordering = ['-created_at']
