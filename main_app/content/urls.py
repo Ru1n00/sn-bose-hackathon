f578a2a3346db44b3d0beae8d9a1baed2f303ca0
Ru1n00 <ruhul.amin.tnt@gmail.com>
2025-05-21 06:35:50 +0600

    feat: Add post detail page

--- main_app/content/urls.py
+++ main_app/content/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     index,
     PostListView,
+    post_detail,
 )
 
 app_name = 'content'
@@ -10,4 +11,5 @@ app_name = 'content'
 urlpatterns = [
     path('', index, name='index'),
     path('posts/', PostListView.as_view(), name='post_list'),
+    path('post/<str:slug>', post_detail, name='post_detail'),
 ]
--- main_app/content/views.py
+++ main_app/content/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView
 
+from .utils import translate_text
 from .models import (
-    Post
+    Post,
+    PostFile,
 )
 
 # Create your views here.
 def index(request):
     return render(request, 'content/index.html')
+
 
 class PostListView(ListView):
     model = Post
@@ -18,3 +21,30 @@ class PostListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+    
+
+def post_detail(request, slug):
+    # Fetch the post by slug
+    post = get_object_or_404(Post, slug=slug)
+
+
+    # Fetch all files related to the post
+    files = PostFile.objects.filter(post=post)
+    images = files.filter(file_type__in=['jpg', 'jpeg', 'png'])
+    videos = files.filter(file_type__in=['mp4', 'mkv'])
+
+    lang = request.GET.get('lang', 'en')
+
+    if lang == 'bn':
+        # Translate the post title and description
+        translated_title = translate_text(post.title)
+        translated_description = translate_text(post.description)
+        post.title = translated_title
+        post.description = translated_description
+        
+    context = {
+        'post': post,
+        'videos': videos,
+        'images': images,
+    }
+    return render(request, 'content/post_detail.html', context)
