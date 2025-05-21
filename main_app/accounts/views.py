ecdf27d0075ca38c828f8d26658e55a56adfd86f
Ru1n00 <ruhul.amin.tnt@gmail.com>
2025-05-22 01:26:56 +0600

    feat: add redirect in login

--- main_app/accounts/views.py
+++ main_app/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.urls import is_valid_path
+from urllib.parse import urlparse
 
 from .forms import UserAuthenticationForm
 
@@ -11,25 +13,30 @@ def sign_in(request):
     if request.user.is_authenticated:
         return redirect('content:index')  # Redirect to home if already authenticated
     
+    next_url = request.GET.get("next", "")
+
     if request.method == 'POST':
         form = UserAuthenticationForm(request, data=request.POST)
-        print(request.POST)
-        print(form)
-        print(form.is_valid())
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
 
+            next_url = request.POST.get('next', '')
             if user is not None:
                 login(request, user)
-                return render(request, 'content/index.html', {'user': user})
+                # Ensure next_url is safe or default to home page
+                parsed_url = urlparse(next_url)
+                if not parsed_url.netloc and is_valid_path(next_url):
+                    return redirect(next_url)
+                else:
+                    return render(request, 'content/index.html', {'user': user})
             else:
                 messages.error(request, "Invalid email or password.")
-                return render(request, 'accounts/sign_in.html', {'form': form})
+                return render(request, 'accounts/sign_in.html', {'form': form, 'next': next_url})
         else:
             messages.error(request, "Invalid email or password.")
-            return render(request, 'accounts/sign_in.html', {'form': form})
+            return render(request, 'accounts/sign_in.html', {'form': form, 'next': next_url})
     else:
         form = UserAuthenticationForm()
-    return render(request, 'accounts/sign_in.html', {'form': form})
+    return render(request, 'accounts/sign_in.html', {'form': form, 'next': next_url})
